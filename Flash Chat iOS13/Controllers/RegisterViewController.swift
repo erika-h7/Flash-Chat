22f7cb1d89e8784f07dd6d8daf6cd22b1997872a
erika-h7 <erika.herrera1@outlook.com>
2022-09-16 13:43:29 -0700

    Connected app to Firebase

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
@@ -14,6 +15,21 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    // for deployment create an error pop up for the user
+                    print(e.localizedDescription)
+                } else {
+                    // Navigate to chat view controller
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
+        
+        
     }
     
 }
+
