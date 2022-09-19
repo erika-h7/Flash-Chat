5c372b7dc6d1bff2216974ca7235acf480da8393
erika-h7 <erika.herrera1@outlook.com>
2022-09-19 11:50:32 -0700

    Created constants file, added model message

---
+++ Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Infinity Code on 9/19/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
